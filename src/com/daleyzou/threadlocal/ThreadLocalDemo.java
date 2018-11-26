d88c83b7fe8e6c492e221ce4674a23656fa60df8
daleyzou <daleyzou@163.com>
2018-11-26 09:42:28 +0800

    学习使用ThreadLocal

---
+++ src/com/daleyzou/threadlocal/ThreadLocalDemo.java
@@ -0,0 +1,57 @@
+package com.daleyzou.threadlocal;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @Author: DaleyZou
+ * @Description: 学习使用ThreadLocal
+ * @Date: Created in 11:42 2018/11/20
+ * @Modified By:
+ */
+public class ThreadLocalDemo {
+    public static void main(String[] args) throws InterruptedException {
+        int threads = 3;
+        CountDownLatch countDownLatch = new CountDownLatch(threads);
+        InnerClass innerClass = new InnerClass();
+        for (int i = 1; i <= threads; i++){
+            new Thread(()->{
+                for (int j = 0; j < 4; j++){
+                    innerClass.add(String.valueOf(j));
+                    innerClass.print();
+                }
+                innerClass.set("daleyzou 加油");
+                countDownLatch.countDown();
+            }, "thread - " + i).start();
+        }
+        countDownLatch.await();
+    }
+    private static class InnerClass{
+        public void add(String newStr){
+            StringBuilder sb = Counter.counter.get();
+            Counter.counter.set(sb.append(newStr));
+        }
+        public void print(){
+            System.out.printf("Thread name: %s, ThreadLocal hashcode: %s, Instance hashcode: %s, Value: %s\n",
+                    Thread.currentThread().getName(),
+                    Counter.counter.hashCode(),
+                    Counter.counter.get().hashCode(),
+                    Counter.counter.get().toString());
+        }
+        public void set(String words){
+            Counter.counter.set(new StringBuilder(words));
+            System.out.printf("Set, Thread name: %s, ThreadLocal hashcode:%s, Instance hashcode:%s, Value:%s\n",
+                    Thread.currentThread().getName(),
+                    Counter.counter.hashCode(),
+                    Counter.counter.get().hashCode(),
+                    Counter.counter.get().toString());
+        }
+    }
+    private static class Counter{
+        private static ThreadLocal<StringBuilder> counter = new ThreadLocal<StringBuilder>(){
+            @Override
+            protected StringBuilder initialValue() {
+                return new StringBuilder();
+            }
+        };
+    }
+}
